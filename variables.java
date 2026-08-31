b66a14cf9f4b94095a6b2035c9311b05f45a6a12
Fahad Iqbal <fahadiqbal1901@gmail.com>
2026-08-31 06:40:52 +0530

    Sum of two numbers

--- variables.java
+++ variables.java
@@ -2,8 +2,10 @@ public class variables{
   public static void main(String args[]){
     int A = 5;
     int B = 10;
+    String name = "Fahad Iqbal";
     System.out.println(A); 
     System.out.println(B); 
+    System.out.println(name);
   }
 
 } 
