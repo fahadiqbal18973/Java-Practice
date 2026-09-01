4f3f6b371fc173bb3f08f1de3412624d55cfdbc0
Fahad Iqbal <fahadiqbal1901@gmail.com>
2026-09-01 12:06:20 +0530

    Average of 3 numbers

---
+++ Average.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Average {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter a: ");
+    int a = sc.nextInt();
+    System.out.print("Enter b: ");
+    int b = sc.nextInt();
+    System.out.print("Enter c: ");
+    int c = sc.nextInt();
+    int average = (a + b + c) / 3;
+    System.out.println("Average of a, b and c is: " + average);
+  }
+  
+}
